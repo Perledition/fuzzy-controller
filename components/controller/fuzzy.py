a59372e72172b35c5b622c64f145efc84fb9c81c
Maxim Perl <maxim.perl@outlook.de>
2020-07-22 21:41:18 +0200

    build(master): FuzzyDistanceController rule subset generation of modus ponens

--- components/controller/fuzzy.py
+++ components/controller/fuzzy.py
@@ -8,8 +8,7 @@ import pandas as pd
 # import project related modules
 from components.controller.membership import MembershipValidator
 
-# TODO: add input values
-# TODO: add ruleset
+
 # TODO: create min_function
 # TODO: create center of gravity for absolut -> softmax with weighted average output
 # TODO: return value
@@ -20,7 +19,7 @@ class FuzzyDistanceController(object):
     def __init__(self):
 
         # feature space / inputs to measure memberships
-        self.feature_space = dict()
+        self.feature_space = None
 
         # rule set applied for inference
         self.rules = None
@@ -43,14 +42,63 @@ class FuzzyDistanceController(object):
             warnings.warn("fuzzification was not successfull, make sure your inputs match your settings")
             return dict()
 
-    def _inference(self, perception):
+    @staticmethod
+    def _query_generator(category_members):
+
+        # generate a query value combinations from category and related values - is needed to keep the query dynamic
+        for cat, members in category_members.items():
+
+            # query string must include a or condition - since a category can have multiple members true
+            # to a certain degree
+            query_string = " | ".join(f"({cat}=='{mem}')" for mem in members)
+            yield query_string
+
+    def _get_rule_subset(self, perception: dict):
+
+        # initialize resulting dict
+        category_members = dict()
+        for category, members in perception.items():
+
+            # set a list for the current category so members can be collected
+            category_members[category] = list()
+            for key, value in members.items():
+                if value > 0:
+                    category_members[category].append(key)
+
+        # create a subset of the given rules - copy complete data set and filter afterwards to ensure
+        # other memory location and prevent overwriting
+        subset = self.rules.loc[:, :]
+
+        # create query conditions and filter the given data set for conditions
+        for query in self._query_generator(category_members):
+            subset = subset.query(query)
+
+        return subset
+
+    def _inference(self, perception: dict):
+        print(perception)
+
+        # filter subset of rules that match the perception - filter because the rest is not needed and can be ignored
+        subset = self._get_rule_subset(perception)
+
+        # iterate over the ruleset and check to which degree modus ponens is true
+        # TODO: calculate min for each rule
+        # TODO: think about a solution how to solve the joint value defuzzification
+
         return dict()
 
     def _defuzzification(self):
         pass
 
-    def add_ruleset(self, rules: pd.DataFrame):
-        pass
+    def set_ruleset(self, rules: pd.DataFrame):
+
+        # set self.rules (required rule set) if object type is a pandas data frame: data frame is used to make
+        # the code more readable and understandable
+        if type(rules) is pd.DataFrame:
+            self.rules = rules
+            return True
+        else:
+            return False
 
     def set_inputs(self, feature_space: dict):
 
@@ -78,59 +126,5 @@ class FuzzyDistanceController(object):
 
 
 
-"""
-input:
-speed -> what is my current speed
-driver type -> how much distance is the driver willing to keep
 
-output: change of distance in m/s
-
-
-"""
-
-target_distance = {
-    "very_low": {"lower_end": 0, "center": 0, "upper_end": 50},
-    "low": {"lower_end": 0, "center": 50, "upper_end": 100},
-    "medium": {"lower_end": 50, "center": 100, "upper_end": 150},
-    "high": {"lower_end": 100, "center": 150, "upper_end": 200},
-    "very_high": {"lower_end": 150, "center": 200, "upper_end": 200}
-}
-
-driver_type = {
-    "aggressive": {"lower_end": 0, "center": 0, "upper_end": 50},
-    "medium": {"lower_end": 25, "center": 50, "upper_end": 75},
-    "defensive": {"lower_end": 50, "center": 75, "upper_end": 100}
-}
-
-change_of_distance = {
-    "strong_negative": {"lower_end": -20, "center": -20, "upper_end": -5},
-    "negative": {"lower_end": -10, "center": -5, "upper_end": 0},
-    "zero": {"lower_end": -5, "center": 0, "upper_end": 5},
-    "positive": {"lower_end": 0, "center": 5, "upper_end": 10},
-    "strong_positive": {"lower_end": 5, "center": 10, "upper_end": 20},
-}
-
-settings = {
-    "target_distance": target_distance,
-    "driver_type": driver_type,
-    "change_of_distance": change_of_distance
- }
-
-current = {"target_distance": 34, "driver_type": 33, "change_of_distance": 6.3}
-
-fc = FuzzyDistanceController()
-fc.set_inputs(settings)
-print(fc.run(current))
-
-# td = MembershipValidator(target_distance)
-# dt = MembershipValidator(driver_type)
-# cod = MembershipValidator(change_of_distance)
-
-# current = (5, 40, 17.5)
-
-# print(td.max, td.min)
-# print(td.category)
-# print(td.get_memberships(5, return_type="dict"))
-# print(dt.get_memberships(40, return_type="dict"))
-# print(cod.get_memberships(17.5, return_type="dict"))
 
--- experiment/simple_car_travel.py
+++ experiment/simple_car_travel.py
@@ -1,6 +1,58 @@
 # import standard modules
 
 # import third party modules
+import pandas as pd
 
 # import project related modules
+from components.controller.fuzzy import FuzzyDistanceController
 
+df = pd.read_csv("ruleset_fuzzy_distance_controller.csv", index_col=0)
+print(df.head())
+
+"""
+input:
+speed -> what is my current speed
+driver type -> how much distance is the driver willing to keep
+
+output: change of distance in m/s
+
+
+"""
+
+ruleset = pd.read_csv("ruleset_fuzzy_distance_controller.csv", index_col=0)
+
+target_distance = {
+    "very low": {"lower_end": 0, "center": 0, "upper_end": 50},
+    "low": {"lower_end": 0, "center": 50, "upper_end": 100},
+    "medium": {"lower_end": 50, "center": 100, "upper_end": 150},
+    "high": {"lower_end": 100, "center": 150, "upper_end": 200},
+    "very high": {"lower_end": 150, "center": 200, "upper_end": 200}
+}
+
+driver_type = {
+    "aggressive": {"lower_end": 0, "center": 0, "upper_end": 50},
+    "medium": {"lower_end": 25, "center": 50, "upper_end": 75},
+    "defensive": {"lower_end": 50, "center": 75, "upper_end": 100}
+}
+
+change_of_distance = {
+    "strong_negative": {"lower_end": -20, "center": -20, "upper_end": -5},
+    "negative": {"lower_end": -10, "center": -5, "upper_end": 0},
+    "zero": {"lower_end": -5, "center": 0, "upper_end": 5},
+    "positive": {"lower_end": 0, "center": 5, "upper_end": 10},
+    "strong_positive": {"lower_end": 5, "center": 10, "upper_end": 20},
+}
+
+settings = {
+    "target_distance": target_distance,
+    "driver_type": driver_type,
+    "change_of_distance": change_of_distance
+}
+
+current = {"target_distance": 34, "driver_type": 33, "change_of_distance": 6.3}
+
+fc = FuzzyDistanceController()
+fc.set_inputs(settings)
+fc.set_ruleset(ruleset)
+
+print(fc.run(current))
